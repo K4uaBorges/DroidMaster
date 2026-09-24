6700fe1caaa2f6a4da12b6bba3fc1d16dea766f0
K4uaBorges <Borgeskaua296@gmail.com>
2026-09-24 12:37:56 +0100

    a

---
+++ app/src/androidTest/java/testApp/DroidMasterTest/storage/dao/MessageDaoTest.kt
@@ -0,0 +1,8 @@
+package testApp.DroidMasterTest.DroidMasterTest.storage.dao
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class MessageDaoTest {
+    @Test fun placeholder() = assertTrue(true)
+}
---
+++ app/src/androidTest/java/testApp/DroidMasterTest/storage/entity/MessageEntityTest.kt
@@ -0,0 +1,8 @@
+package testApp.DroidMasterTest.DroidMasterTest.storage.entity
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class MessageEntityTest {
+    @Test fun placeholder() = assertTrue(true)
+}
